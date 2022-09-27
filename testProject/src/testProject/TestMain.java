3518c6aa56a7ba76e7a7d5d422308f773b935d0f
Noah Rettig <noah.rettig.s@gmail.com>
2022-09-27 10:26:17 -0500

    Added dancing and saying goodbye

--- testProject/src/testProject/TestMain.java
+++ testProject/src/testProject/TestMain.java
@@ -2,6 +2,7 @@ package testProject;
 
 public class TestMain {
 	public static void main(String[] args) {
-		System.out.print("Hello Chicken World!");
+		System.out.println("Hello Chicken World!");
+		System.out.println("Goodbye.");
 	}
 }
